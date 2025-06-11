e233a0a338ae19229fd350ff17779008b6aff943
DonnieD-tech <vereitd@gmail.com>
2025-06-11 22:00:38 +0300

    feat(dog_api): добавлен класс BreedViewSet

--- Theme 3/main_api/views.py
+++ Theme 3/main_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models import Count, OuterRef, Subquery
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Breed, Dog
+from .serializers import BreedSerializer, DogSerializer
+
+
+class BreedViewSet(viewsets.ModelViewSet):
+    """ViewSet для управления объектами модели Breed.
+
+    Позволяет выполнять операции:
+    - Получение списка пород
+    - Создание новой породы
+    - Получение, обновление и удаление конкретной породы
+    """
+
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+    def get_queryset(self):
+        """Возвращает queryset с аннотацией dog_count — количество собак данной породы."""
+
+        return Breed.objects.annotate(dog_count=Count("dogs"))
