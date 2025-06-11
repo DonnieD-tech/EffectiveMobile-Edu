f86b6145cc5d4c5079962101cb1112c2a84edf17
DonnieD-tech <vereitd@gmail.com>
2025-06-11 21:15:10 +0300

    feat(dog_api): добавлены сериалайзеры DogSerializer и BreedSerializer

---
+++ Theme 3/main_api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from .models import Breed, Dog
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Breed."""
+
+    dog_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Breed
+        fields = "__all__"
+
+
+class DogSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Dog."""
+
+    breed_name = serializers.CharField(source="breed.name", read_only=True)
+    same_breed_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Dog
+        fields = [
+            "id",
+            "name",
+            "age",
+            "breed",
+            "gender",
+            "color",
+            "favorite_food",
+            "favorite_toy",
+            "breed_name",
+            "same_breed_count",
+        ]
