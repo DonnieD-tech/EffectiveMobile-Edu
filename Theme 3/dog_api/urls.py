1a0e7d95646d40dc369bba41d13b820a5bd1372e
DonnieD-tech <vereitd@gmail.com>
2025-06-12 09:02:13 +0300

    feat(dog_api): решена проблема с абсолютным импортом

--- Theme 3/dog_api/urls.py
+++ Theme 3/dog_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from ..main_api.views import BreedViewSet, DogViewSet
+from main_api.views import BreedViewSet, DogViewSet
 
 
 router = DefaultRouter()
