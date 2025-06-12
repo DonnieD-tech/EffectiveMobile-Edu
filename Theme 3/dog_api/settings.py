81425a1d7306d7cb7b001c3f8bd0f66416d74d3d
DonnieD-tech <vereitd@gmail.com>
2025-06-12 08:57:29 +0300

    feat(dog_api): добавлен объект router в urls

--- Theme 3/dog_api/settings.py
+++ Theme 3/dog_api/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.append(str(BASE_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'main_api'
+    'main_api',
 ]
 
 MIDDLEWARE = [
